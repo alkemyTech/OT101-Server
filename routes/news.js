cb204804516d6fa67c52a343aded643ce1a288e8
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-11-24 13:13:03 -0300

    finish delete news endpoint

--- routes/news.js
+++ routes/news.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const New = require('../models/new'); //Puede cambiar el nombre del model
 
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const newObj = await New.findOne({ where: { id } });
+  if (newObj) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 module.exports = router;
