76c63dfda3489ee5647ce2ca54c8b85bcd8ff6dc
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-11-25 16:47:57 -0300

    Add error handling

--- routes/news.js
+++ routes/news.js
@@ -4,11 +4,17 @@ const New = require('../models/new'); //Puede cambiar el nombre del model
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  const newObj = await New.findOne({ where: { id } });
-  if (newObj) {
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(404);
+  try {
+    const newObj = await New.findOne({ where: { id } });
+    if (newObj) {
+      await newObj.destroy();
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
   }
 });
 
