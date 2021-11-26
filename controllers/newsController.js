56cf76c6d8be3d6776fab91d88d86bdaf7e74b4f
Pablo Menichini <pablo.menichini@gmail.com>
2021-11-26 00:39:53 -0300

    implement news listing endpoint

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,6 +1,17 @@
+const { Entry } = require('../models');
 
 module.exports = {
     index: function(req, res) {
-        res.send('list of news');
+        Entry.findAll({
+            attributes: ['name', 'image', 'createdAt'],
+            where: {
+                type: 'news'
+            }
+        })
+            .then(entries => res.json(entries))
+            .catch(err => {
+                console.log(err);
+                res.sendStatus(500);
+            });
     }
 }
