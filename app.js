c7dee57efefee87ae18a9bc504c9baad1d39ec29
Nico <nico.cagiao@gmail.com>
2021-11-26 15:04:01 -0300

    Deleted extra lines and files

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config()
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const regRouter = require('./routes/register');
+
 const membersRouter = require('./routes/members')
 
 const app = express();
@@ -26,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/auth/register', regRouter);
 app.use('/members', membersRouter);
 
 // catch 404 and forward to error handler
