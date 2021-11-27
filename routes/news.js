63b0b0e9579a4b7d017172a92a87c1907777d30b
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-11-27 01:46:06 -0300

    Ot101-63 Endpoint eliminar novedades (#2)
    * Create news route
    
    * Add news router to express app
    
    * finish delete news endpoint
    
    * Add error handling
    
    * a comment deleted
    
    Co-authored-by: hfoncea <hfoncea@yahoo.com>

--- routes/news.js
+++ routes/news.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const New = require('../models/new');
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const newObj = await New.findOne({ where: { id } });
+    if (newObj) {
+      await newObj.destroy();
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
 
 const newsController = require('../controllers/newsController');
 
