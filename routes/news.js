06ebd1c8e1281d9ad47f404606b2457a0b31befa
Pablo Menichini <pablo.menichini@gmail.com>
2021-11-27 20:52:21 -0300

    add image handling & storage in S3

--- routes/news.js
+++ routes/news.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const New = require('../models/new');
+const { s3 } = require('../services/aws');
+const multer    = require('multer');
+const multerS3  = require('multer-s3');
+const { v4: uuidv4 } = require('uuid');
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: (req, file, cb) => {
+      cb(null, uuidv4())
+    }
+  }),
+});
+
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
@@ -24,9 +40,13 @@ const { newsValidation, validationHandler } = require('../middlewares/newsValida
 /* GET news listing. */
 router.get('/', newsController.index);
 
-/* POST news creation. */
+/*
+  POST news creation.
+  form-data expected
+*/
 router.post(
   '/',
+  upload.single('image'),
   newsValidation,
   validationHandler,
   newsController.create
