796481a916c995f8fc0b157d91edf2f772083434
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-01 04:39:13 -0300

    OT101-40 Entries model (#13)
    * Entries model created
    
    * move delete new endpoint to news controller
    
    * Organize news router file
    
    * create entries migration
    
    * change model in delete news endpoint

--- routes/news.js
+++ routes/news.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const New = require('../models/new');
 const { s3 } = require('../services/aws');
-const multer    = require('multer');
-const multerS3  = require('multer-s3');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
 const { v4: uuidv4 } = require('uuid');
+const newsController = require('../controllers/newsController');
+const { newsValidation, validationHandler } = require('../middlewares/newsValidator');
 
 const upload = multer({
   storage: multerS3({
@@ -12,44 +13,13 @@ const upload = multer({
     bucket: process.env.AWS_BUCKET,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (req, file, cb) => {
-      cb(null, uuidv4())
-    }
+      cb(null, uuidv4());
+    },
   }),
 });
 
-
-router.delete('/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-    const newObj = await New.findOne({ where: { id } });
-    if (newObj) {
-      await newObj.destroy();
-      res.sendStatus(200);
-    } else {
-      res.sendStatus(404);
-    }
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
-});
-
-const newsController = require('../controllers/newsController');
-const { newsValidation, validationHandler } = require('../middlewares/newsValidator');
-
-/* GET news listing. */
 router.get('/', newsController.index);
-
-/*
-  POST news creation.
-  form-data expected
-*/
-router.post(
-  '/',
-  upload.single('image'),
-  newsValidation,
-  validationHandler,
-  newsController.create
-);
+router.post('/', upload.single('image'), newsValidation, validationHandler, newsController.create);
+router.delete('/:id', newsController.delete);
 
 module.exports = router;
