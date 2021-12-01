831219bfd4117f350a1014496bbedaa3880706e2
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-01 02:49:06 -0300

    remove req.user.isAdmin

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -3,10 +3,7 @@ const { User, Role } = require('../models');
 module.exports = async function isAdmin(req, res, next) {
   if (req.user) {
     const user = await User.findOne({ where: { id: req.user.id }, include: { model: Role, as: 'role' } });
-    if (user && user.role?.name === 'Admin') {
-      req.user.isAdmin = true;
-      return next();
-    }
+    if (user && user.role?.name === 'Admin') return next();
   }
   res.sendStatus(401);
 };
