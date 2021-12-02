345d7219431fb018524d80b5dab912de3e2959f3
Nico <nico.cagiao@gmail.com>
2021-12-02 09:58:00 -0300

    members model creation and members validation

--- routes/members.js
+++ routes/members.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const { Member } = require('../models/members.js');
+
 const { body, validationResult } = require('express-validator');
 
 /* Post members. */
@@ -8,13 +10,17 @@ router.post('/',
 // name must be a string
   body('name').isString(),
   (req, res) => {
-    // Finds the validation errors in this request and wraps them in an object with handy functions
+    // Finds the validation errors in this request and wraps them in an object
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
         {
-            res.json('miembro ok')
+            Member.create({
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              image: req.body.image
+            }).then(member => res.json(member));
     }
 
   },
