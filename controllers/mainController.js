a59c4ed84e2c1b452eb26c97caa0befe013320c5
alejandropascuale <apascuale@outlook.com>
2021-12-02 13:21:51 -0300

    create controller to store data from contact form

---
+++ controllers/mainController.js
@@ -0,0 +1,17 @@
+const db = requiere('../models');
+
+const controller = {
+    storeContact: async (req, res) => {
+        await db.Contacts.create({
+            name: req.body.name,
+            lastname: req.body.lastname,
+            phone: req.body.phone,
+            email: req.body.email,
+            message: req.body.content,
+            deleteAt: new Date()
+        })
+        return res.redirect('/');
+    }
+}
+
+module.exports = controller;
