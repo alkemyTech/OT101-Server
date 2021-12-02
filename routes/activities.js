d4dc2b842bdbf6766c82b19ed901d8cbc948f9c6
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-02 14:27:41 -0300

    add endpoint validator

--- routes/activities.js
+++ routes/activities.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const activitiesController = require('../controllers/activitiesController');
+const { activitiesValidator, validationHandler } = require('../middlewares/activitiesValidator');
 
-router.post('/', activitiesController.create);
+router.post('/', activitiesValidator, validationHandler, activitiesController.create);
 
 module.exports = router;
