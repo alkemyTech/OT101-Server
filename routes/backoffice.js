5f5c45ea05334338308f07de9030eea8226af6d2
alejandropascuale <apascuale@outlook.com>
2021-12-02 13:21:09 -0300

    add route post to store contact

--- routes/backoffice.js
+++ routes/backoffice.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
+const backofficeController = require('../controllers/backofficeController');
 
-router.get('/contacts');
+router.get('/contacts', backofficeController.listContacts);
 
 module.exports = router;
