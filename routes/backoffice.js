55fa27ebde5848eb62502bb869e361ed8f5dbfb1
alejandropascuale <apascuale@outlook.com>
2021-12-03 10:29:18 -0300

    add check rol user and controller to get contacts path

--- routes/backoffice.js
+++ routes/backoffice.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const adminMiddleware = require ('../middlewares/isAdmin');
+const backofficeController = require('../controllers/backofficeController');
 
-router.get('/contacts');
+router.get('/contacts', adminMiddleware, backofficeController.listContacts);
 
 module.exports = router;
