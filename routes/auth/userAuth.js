e5c64f578af964d619df81aae068213baa54cb79
germansanz93 <germansanz93@gmail.com>
2021-12-03 11:30:10 -0300

    more refactoring

--- routes/auth/userAuth.js
+++ routes/auth/userAuth.js
@@ -8,22 +8,19 @@ const saltRounds = 10;
 
 /* Register new user endpoint. */
 
-const registerValidations = [
-    // username must be an email
-    body('username').isEmail(),
-    // password must be at least 5 chars long
-    body('password').isLength({ min: 5 }),
-    //validate name and last name
-    body('firstName').isString(),
-    body('lastName').isString(),
-]
-
 const loginValidations = [
     // username must be an email
     body('username').isEmail(),
     // password must be at least 5 chars long
     body('password').isLength({ min: 5 }),
 ]
+const registerValidations = [
+    ...loginValidations,
+    //validate name and last name
+    body('firstName').isString(),
+    body('lastName').isString(),
+]
+
 
 router.post('/register',
     registerValidations,
