4342f909fdb4d5414da361b2b4bf4782439c390e
Pablo Menichini <pablo.menichini@gmail.com>
2021-12-03 20:49:16 -0300

    move multer-s3 to aws service

--- services/aws.js
+++ services/aws.js
@@ -1,4 +1,7 @@
 const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const { v4: uuidv4 } = require('uuid');
 
 const s3 = new aws.S3({
     region: process.env.AWS_REGION,
@@ -6,21 +9,41 @@ const s3 = new aws.S3({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
-// file: valid object from multer-s3 req.file
-const s3deleteFile = (file) => {
+/*
+    s3multerUpload is a multer instance preconfigured to upload one file directly to S3
+    Just use it as: s3multerUpload.single(NAME_ATTRIBUTE)
+*/
+const s3multerUpload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.AWS_BUCKET,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: (req, file, cb) => {
+            const fileExt = file.mimetype.split("/").pop();
+
+            cb(null, uuidv4() + '.' + fileExt);
+        },
+    }),
+});
+  
+
+const s3deleteFile = (key) => {
     const params = {
-        Bucket: file.bucket,
-        Key: file.key
+        Bucket: process.env.AWS_BUCKET,
+        Key: key
     };
 
-    s3.deleteObject(params, (err, data) => {
-        if (err) {
-            console.log(err);
-        }
+    return new Promise((resolve, reject) => {
+        s3.deleteObject(params, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        });
     });
 }
 
 module.exports = {
-    s3,
+    s3multerUpload,
     s3deleteFile
 };
