bbe6330b5c72305b84b217b3e59858b2e08928ee
Alejandro Pascuale <83843450+alejandropascuale@users.noreply.github.com>
2021-12-03 10:06:18 -0300

    Delete backoffice.js
    Elimino archivos que quedaron de otros cambios

--- routes/backoffice.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const backofficeController = require('../controllers/backofficeController');
-
-router.get('/contacts', backofficeController.listContacts);
-
-module.exports = router;
