a28855f08485a1ce30bc480276a28578ee9d272c
Francisco Messina <francisco@messina.ws>
2021-12-04 19:57:38 -0300

    User list endpoint completed

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers/userController')
+const isAdmin = require('../middlewares/isAdmin')
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', isAdmin ,userController.listUsers);
 
 module.exports = router;
