22fe70590fab8a8a4ed55d36293510b2cb3e2d76
Alejandro Pascuale <83843450+alejandropascuale@users.noreply.github.com>
2021-12-08 15:59:34 -0300

    Ot101 76 listado de contactos (#33)
    * create backoffice route and controller
    
    * fix error in Entry model
    
    * add check rol user and controller to get contacts path
    
    * add listContacts method in backoffice controller to get all contacts
    
    * change name to json file to response
    
    * delete .DS_Store file

---
+++ controllers/backofficeController.js
@@ -0,0 +1,11 @@
+const db = requiere('../models');
+
+
+const controller = {
+    listContacts: async (req, res) => {
+        const ContactsList = await db.Contacts.findAll();
+        return res.json(ContactsList);
+    }
+}
+
+module.exports = controller;
