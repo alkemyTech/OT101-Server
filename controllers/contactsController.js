480c0ce1b62cc502d39886237bdcf0262489e487
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-09 20:33:37 -0300

    Remove contact lastname field

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -2,9 +2,9 @@ const { Contacts } = require('../models');
 
 module.exports = {
   create: async (req, res) => {
-    const { name, lastname, email, phone, message } = req.body;
+    const { name, email, phone, message } = req.body;
     try {
-      await Contacts.create({ name, lastname, email, phone, message });
+      await Contacts.create({ name, email, phone, message });
       res.sendStatus(200);
     } catch (err) {
       console.error(err);
