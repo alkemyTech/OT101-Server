c6f345e726a2bdd964f3ba328d12962a1b6eb3b0
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-09 12:55:29 -0300

    Add contact lastname field

--- middlewares/contactsValidator.js
+++ middlewares/contactsValidator.js
@@ -2,6 +2,7 @@ const { body } = require('express-validator');
 
 const contactsValidator = [
   body('name').notEmpty().isLength({ max: 255 }).withMessage('Enter a valid name'),
+  body('lastname').notEmpty().isLength({ max: 255 }).withMessage('Enter a valid last name'),
   body('email').isEmail().withMessage('Enter a valid email'),
   body('message').notEmpty().isLength({ min: 5, max: 500 }).withMessage('Enter a valid message'),
 ];
