1be576dd3a1c3528615e697de6b0f6d459ac366d
Nico <nico.cagiao@gmail.com>
2021-12-10 11:40:57 -0300

    duplicated lines in app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use('/news', newsRouter);
+
 app.use('/members', membersRouter);
 app.use('/auth/register', regRouter);
 app.use('/organizations', organizationsRouter)
