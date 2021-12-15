8588fb29cf71c341a66b2849d69ad5e1bc137217
alejandropascuale <apascuale@outlook.com>
2021-12-15 08:50:31 -0300

    add get route to list all members

--- routes/members.js
+++ routes/members.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const isAdmin = require('../middlewares/isAdmin');
 
 const { membersValidation, validationHandler } = require('../middlewares/membersValidator');
 const membersController = require('../controllers/membersController')
 
+router.get('/', isAdmin, membersController.list);
 router.post('/', membersValidation, validationHandler, membersController.create);
 
 module.exports = router;
