d6e0d86c5f7cb3b240281e4199b92fd7d39ca8f4
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-15 19:34:51 -0300

    Delete backoffice endpoint

--- routes/backoffice.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const isAdmin = require ('../middlewares/isAdmin');
-const backofficeController = require('../controllers/backofficeController');
-const { verifyToken } = require("../middlewares/authJWT");
-
-router.get('/contacts', verifyToken, isAdmin, backofficeController.listContacts);
-
-module.exports = router;
