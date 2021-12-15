49dc03672bccd014105a0e66a9c29bc9a3603a1f
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-15 01:24:00 -0300

    91 /members/:id put endpoint (#45)
    * Change Members models fields
    * Add members PUT and multer middleware to POST
    * Add update method and edit create method

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -1,12 +1,33 @@
 const { Member } = require('../models/members');
 
 module.exports = {
-    create: function (req,res) {
-        Member.create({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            image: req.body.image
-          }).then(member => res.json(member));
+  create: (req, res) => {
+    Member.create({
+      name: req.body.name,
+      image: req.file.location,
+    })
+      .then((member) => res.json(member))
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  },
+  update: async (req, res) => {
+    const { id } = req.params;
+    try {
+      let member = await Member.findByPk(id);
+      if (member) {
+        member = await Member.update({
+          name: req.body.name,
+          image: req.file ? req.file.location : member.image,
+        });
+        res.json(member);
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(500);
     }
+  },
 };
-
