4eb15bbebbcc39c5b13cd28d7bcfdcd8052aaa1e
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2021-12-15 01:42:16 -0300

    Refactor PUT /news/:id

--- routes/news.js
+++ routes/news.js
@@ -5,12 +5,13 @@ const { s3multerUpload } = require('../services/aws');
 const newsController = require('../controllers/newsController');
 const { newsValidation } = require('../middlewares/newsValidator');
 const s3validationHandler = require('../middlewares/s3validatorMiddleware');
-
+const { verifyToken } = require('../middlewares/authJWT');
+const isAdmin = require('../middlewares/isAdmin');
 
 router.get('/', newsController.index);
 router.post('/', s3multerUpload.single('image'), newsValidation, s3validationHandler, newsController.create);
-router.get('/:id', newsController.details)
+router.get('/:id', newsController.details);
 router.delete('/:id', newsController.delete);
-router.put('/:id', newsController.update)
+router.patch('/:id', verifyToken, isAdmin, s3multerUpload.single('image'), newsController.update);
 
 module.exports = router;
