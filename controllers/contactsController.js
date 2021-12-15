a6d1a97b05f79405e7accf4bf10b13736863febe
Fran Messina <91616369+FranciscoMessina@users.noreply.github.com>
2021-12-14 21:40:07 -0300

    Ot101 73 endpoint para listar contactos (#29)
    * User list endpoint completed
    
    * endpoint for listing contacts if you are admin.

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,7 +1,12 @@
 const { Contacts } = require('../models');
 
 module.exports = {
-  create: async (req, res) => {
+    listUsers: async (req, res) => {
+        let contacts = await Contacts.findAll()
+
+        res.status(200).json(contacts)
+    },
+    create: async (req, res) => {
     const { name, email, phone, message } = req.body;
     try {
       const contact = await Contacts.create({ name, email, phone, message });
