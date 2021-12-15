b3228e0968eadad0561ce636bfa93df7c259cfbc
alejandropascuale <apascuale@outlook.com>
2021-12-15 08:49:28 -0300

    create list property to list all members

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -1,6 +1,12 @@
 const { Member } = require('../models/members');
 
 module.exports = {
+    list: async (req, res) => {
+        const members = await Member.findAll({
+            attributes: ['firstName', 'lastName', 'image']
+        });
+        return res.json(members);
+    },
     create: function (req,res) {
         Member.create({
             firstName: req.body.firstName,
