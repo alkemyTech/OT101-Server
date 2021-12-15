9b29b6f581beefe546a5fa5e1b5d4e25c2981c5f
Pablo Menichini <pablo.menichini@gmail.com>
2021-12-15 12:20:36 -0300

    validate admin in route

--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const categoriesController = require('../controllers/categoriesController');
 const isAdmin = require('../middlewares/isAdmin');
+const { verifyToken } = require('../middlewares/authJWT');
 
 const router = express.Router();
 
 
 /* GET categories listing. */
-router.get('/', categoriesController.list);
+router.get('/', verifyToken, isAdmin, categoriesController.list);
 
 module.exports = router;
