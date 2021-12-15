cf7c0be09192a857236e3d1518964574e3d69d94
Fran Messina <91616369+FranciscoMessina@users.noreply.github.com>
2021-12-14 21:36:41 -0300

    Ot101 53 listar usuarios (#31)
    * Adding route to handle put requests for news
    
    * Terminado logica de update de novedades.
    
    * Finished and tested PUT to update news, found some errors with other things
    
    * User list endpoint completed
    
    * Updated newsController
    
    Undid a modification that was not correct in the news controller

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -64,4 +64,42 @@ module.exports = {
       res.sendStatus(500);
     }
   },
+  update: async (req, res) => {
+
+    const { id } = req.params
+
+    try {
+      const existingEntry = await Entry.findByPk(id)
+
+      if(!existingEntry) {
+        return res.status(404).json({
+          error: 'Entry not found'
+        })
+      }
+
+      // Destructure values from the body of the request
+      const { name, image, type, categoryId } = req.body
+
+
+      // If new information has been sent update, if not keep old data.
+      existingEntry.name = name || existingEntry.name
+      existingEntry.image = image || existingEntry.image
+      existingEntry.type = type || existingEntry.type
+      existingEntry.categoryId = categoryId || existingEntry.categoryId
+
+      // Save changes to database
+      await existingEntry.save()
+
+      return res.status(200).json({
+        entry: existingEntry
+      })
+
+
+      
+    } catch(error) {
+      console.log(error);
+      res.sendStatus(500)
+    }
+
+  }
 };
