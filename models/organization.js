ff8e8aa9464002d7bc24d111eb5fd94c841b50c1
Francisco Messina <francisco@messina.ws>
2021-12-18 16:08:43 -0300

    creo modelo y migracion de organizacion

---
+++ models/organization.js
@@ -0,0 +1,31 @@
+"use strict"
+const { Model } = require("sequelize")
+module.exports = (sequelize, DataTypes) => {
+	class Organization extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	Organization.init(
+		{
+			name: DataTypes.STRING,
+			image: DataTypes.STRING,
+			phone: DataTypes.STRING,
+			address: DataTypes.STRING,
+			welcomeText: DataTypes.STRING,
+			facebook: DataTypes.STRING,
+			instagram: DataTypes.STRING,
+			linkedin: DataTypes.STRING,
+		},
+		{
+			sequelize,
+			modelName: "Organization",
+		}
+	)
+	return Organization
+}
