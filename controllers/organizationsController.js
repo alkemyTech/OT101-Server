c142f9dc7cf28653ad51b84fb9c2f9fe91b7d23d
Pablo Menichini <pablo.menichini@gmail.com>
2022-01-03 01:43:14 -0300

    add home update endpoint

--- controllers/organizationsController.js
+++ controllers/organizationsController.js
@@ -1,23 +1,74 @@
-const { Organization } = require("../models")
+const { sequelize, Sequelize, Organization, Slide } = require("../models")
 
 const controller = {
 	index: async function (req, res, next) {
 		let { id } = req.params
-		// Find organization with ID from params
-		const org = await Organization.findByPk(id)
 
-		// Return a JSON with the information found
+		try {
+			// Find organization with ID from params
+			const org = await Organization.findByPk(
+				id,
+				{
+					attributes: { exclude: ['id', 'createdAt', 'updatedAt']},
+					include: {
+						model: Slide,
+						attributes: ['imageURL', 'text', 'order']
+					}
+				}
+			);
 
-		res.json({
-			name: org.name,
-			image: org.image,
-			phone: org.phone,
-			address: org.address,
-			welcomeText: org.welcomeText,
-			facebook: org.facebook,
-			instagram: org.instagram,
-			linkedin: org.linkedin,
-		})
+			if (!org) {
+				return res.send(404);
+			}
+			// Return a JSON with the information found
+			res.json(org);
+		} catch (error) {
+			console.log(error);
+			res.status(500);
+		};
+	},
+	update: async (req, res) => {
+		const { id } = req.params;
+		const { welcomeText, img1text, img2text, img3text } = req.body;
+
+		const img1 = req.files.img1[0].location;
+		const img2 = req.files.img2[0].location;
+		const img3 = req.files.img3[0].location;
+
+		if (!welcomeText.length || !img1 || !img2 || !img3) {
+			return res.status(400);
+		}
+
+		const t = await sequelize.transaction();
+		try {
+			await Organization.update(
+				{ id, welcomeText },
+				{ where: { id: id}, transaction: t }
+			);
+
+			await Slide.destroy({
+				where: { [Sequelize.Op.or]: [{order: 1}, {order: 2}, {order: 3}] },
+				transaction: t,
+			});
+
+			await Slide.bulkCreate(
+				[
+					{ imageUR1L: img1, text: img1text, order: 1, organizationID: id },
+					{ imageURL: img2, text: img2text, order: 2, organizationID: id },
+					{ imageURL: img3, text: img3text, order: 3, organizationID: id }
+				],
+				{ transaction: t }
+			);
+
+			await t.commit();
+
+			return res.json({});
+		} catch (error) {
+			await t.rollback();
+
+			console.log(error);
+			return res.status(500);
+		}
 	},
 }
 
--- routes/organizations.js
+++ routes/organizations.js
@@ -1,7 +1,14 @@
 var express = require("express")
 var router = express.Router()
 const organizationsController = require("../controllers/organizationsController")
+const { s3multerUpload } = require('../services/aws');
+
 /* GET home page. */
 router.get("/:id/public", organizationsController.index)
+router.put("/:id/public", s3multerUpload.fields([
+    {  name: 'img1', maxCount: 1 },
+    {  name: 'img2', maxCount: 1 },
+    {  name: 'img3', maxCount: 1 },
+]), organizationsController.update);
 
 module.exports = router
